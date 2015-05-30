0de923f34f21c9d948ab624c232c8aee778c13c4
Christian Evans <christian.evans214@gmail.com>
2015-05-30 11:38:11 -0400

    completed my first ASYNC I_O

--- myFirstI_O.js
+++ myFirstI_O.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
 buf = fs.readFileSync(process.argv[2]);
-var bufSplit = buf.toString().split("\n");
-console.log(bufSplit.length-1);
+var bufSplit = buf.toString().split("\n").length-1;
+console.log(bufSplit);
 
