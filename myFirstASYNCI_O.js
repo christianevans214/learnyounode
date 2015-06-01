c287d3a81e59b19c6303c7185e84fbd9d96d3c61
Christian Evans <christian.evans214@gmail.com>
2015-06-01 08:27:59 -0400

    Update myFirstASYNCI_O.js

--- myFirstASYNCI_O.js
+++ myFirstASYNCI_O.js
@@ -3,6 +3,6 @@
 var fs = require('fs');
 var file = process.argv[2]
 
-buf = fs.readFile(file, function fun(err, buffer){
+fs.readFile(file, function fun(err, buffer){
 	console.log(buffer.toString().split("\n").length-1);
 });
